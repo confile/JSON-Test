a0791dd788fec271bb2338c7bff14580367b4b88
Michael Gorski <info@michaelgorski.de>
2015-02-10 03:30:17 +0100

    test

---
+++ src/main/java/test/client/GWTT.java
@@ -0,0 +1,16 @@
+package test.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+
+
+
+
+public class GWTT implements EntryPoint {
+
+
+	public void onModuleLoad() {
+
+		GWT.log("sdfsdf12");
+	}
+}
